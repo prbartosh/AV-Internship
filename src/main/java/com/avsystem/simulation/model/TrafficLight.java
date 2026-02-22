00d0a60150a754e2ae2c0cc912ab295079f7ffe2
prbartosh <pajorb4@gmail.com>
2026-02-22 11:17:42 +0100

    TrafficLight

--- src/main/java/com/avsystem/simulation/model/TrafficLight.java
+++ src/main/java/com/avsystem/simulation/model/TrafficLight.java
@@ -1,4 +1,26 @@
 package com.avsystem.simulation.model;
 
+// TrafficLight przechowuje stan światła i liczbę kroków w obecnym stanie
+// z TrafficLight można odczytać obecny kolor światła i info czy można jechać
 public class TrafficLight {
+    private LightState state;
+    private int stepsInCurrentState;
+
+    public TrafficLight() {
+        this.state = LightState.RED;
+        this.stepsInCurrentState = 0;
+    }
+
+    public void setState(LightState state) {
+        this.state = state;
+        this.stepsInCurrentState = 0;
+    }
+
+    public void tick() {
+        this.stepsInCurrentState++;
+    }
+
+    public LightState getState() { return this.state; }
+    public int getStepsInCurrentState() { return this.stepsInCurrentState;}
+    public boolean allowsPassage() { return this.state.allowsPassage();  }
 }
