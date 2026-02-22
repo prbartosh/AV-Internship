f37f0f3d7a33e9dd77fa0462e6de4d3d044a0c4f
prbartosh <pajorb4@gmail.com>
2026-02-22 10:29:45 +0100

    RoadDirection.java

--- src/main/java/com/avsystem/simulation/model/RoadDirection.java
+++ src/main/java/com/avsystem/simulation/model/RoadDirection.java
@@ -1,4 +1,19 @@
 package com.avsystem.simulation.model;
 
 public enum RoadDirection {
+    NORTH, SOUTH, EAST, WEST;
+
+    public RoadDirection opposite() {
+        return switch (this) {
+            case NORTH -> SOUTH;
+            case SOUTH -> NORTH;
+            case EAST -> WEST;
+            case WEST -> EAST;
+        };
+    }
+
+    // do parsowania json'a
+    public static RoadDirection fromString(String s) {
+        return valueOf(s.toUpperCase());
+    }
 }
